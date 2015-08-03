b8c66a3e0daca1fa69ff0d360a9aa688ea151476
Priya Patil <priya10487@aol.com>
2015-08-03 21:16:38 +0100

    Removed unnecessary input from ComputerPlayer tests.

--- spec/computer_player_spec.rb
+++ spec/computer_player_spec.rb
@@ -4,13 +4,11 @@ describe ComputerPlayer do
 
   it "doesn't return an index when it can only make a move that results in a tie" do 
     cells = ['x','x','o','o','o','x','x','o','x']
-    input = StringIO.new("1\n2\n")
     expect(ComputerPlayer.new(cells).computer_player).to eq([])
   end
 
   it 'returns an index if it is a winning move' do
     cells = ['o','x','o','x',' ','x','o','o',' ']
-    input = StringIO.new("1\n2\n")
     expect(ComputerPlayer.new(cells).computer_player).to eq([4])
 
 
